20774a9be018b5c123b2f511541cd50d2e127e30
drinkingsweets <117858181+drinkingsweets@users.noreply.github.com>
2025-02-28 16:23:06 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("gg.jte.gradle") version "3.1.12"
+}
+
+group = "app"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+//    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+//    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("gg.jte:jte:3.1.12")
+    implementation("gg.jte:jte-spring-boot-starter-3:3.1.12")
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("org.postgresql:postgresql")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+jte {
+    generate()
+    binaryStaticContent = true
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
